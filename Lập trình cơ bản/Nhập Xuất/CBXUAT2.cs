8f4190eefbdb47cb46fb61efa02dbc9b5fd2d8c6
MrT223 <148790555+MrT223@users.noreply.github.com>
2025-06-13 14:47:43 +0700

    Xuất cơ bản 2

---
+++ Lập trình cơ bản/Nhập Xuất/CBXUAT2.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Co gang hoc lap trinh tot - nhung dao duc phai tot nua");
+        Console.WriteLine("\"Vi tuong lai\"");
+        Console.WriteLine("'Tat ca co len'");
+        Console.WriteLine();
+        Console.WriteLine("Ghi nho cac ky tu va lenh dac biet:");
+        Console.WriteLine("\\n - xuong hang");
+        Console.WriteLine("endl - xuong hang");
+        Console.Write("\\t - tab");
+    }
+}
